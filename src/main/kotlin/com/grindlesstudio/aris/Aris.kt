a658f5b2b615382daadc3532000d854c41f0197a
Tenakt <yar.zh.2012@gmail.com>
2026-08-24 15:35:02 +0200

    upd

--- src/main/kotlin/com/grindlesstudio/aris/Aris.kt
+++ src/main/kotlin/com/grindlesstudio/aris/Aris.kt
@@ -9,6 +9,7 @@ import net.fabricmc.fabric.api.biome.v1.BiomeSelectors
 import net.minecraft.registry.RegistryKey
 import net.minecraft.registry.RegistryKeys
 import net.minecraft.util.Identifier
+import net.minecraft.world.biome.BiomeKeys
 import net.minecraft.world.gen.GenerationStep
 import net.minecraft.world.gen.feature.PlacedFeature
 import org.slf4j.LoggerFactory
@@ -35,7 +36,20 @@ object Aris : ModInitializer {
 		)
 
 		BiomeModifications.addFeature(
-			BiomeSelectors.foundInOverworld(),
+			BiomeSelectors.includeByKey(
+				BiomeKeys.PLAINS,
+				BiomeKeys.TAIGA,
+				BiomeKeys.FOREST,
+				BiomeKeys.BIRCH_FOREST,
+				BiomeKeys.OLD_GROWTH_BIRCH_FOREST,
+				BiomeKeys.SNOWY_TAIGA,
+				BiomeKeys.SNOWY_PLAINS,
+				BiomeKeys.MEADOW,
+				BiomeKeys.GROVE,
+				BiomeKeys.JAGGED_PEAKS,
+				BiomeKeys.FROZEN_PEAKS,
+				BiomeKeys.STONY_PEAKS
+			),
 			GenerationStep.Feature.VEGETAL_DECORATION,
 			pebblePatchKey
 		)
